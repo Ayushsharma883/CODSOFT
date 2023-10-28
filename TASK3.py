d2c6390fed0c5cb6c4444e155eccded00f343a56
Ayushsharma883 <149224210+Ayushsharma883@users.noreply.github.com>
2023-10-28 12:30:44 +0530

    Add files via upload

--- TASK3.py
+++ TASK3.py
@@ -1,91 +1,45 @@
-
+import string
 import random
 
 
-print('Winning rules of the game ROCK PAPER SCISSORS are :\n'
-      + "Rock vs Paper -> Paper wins \n"
-      + "Rock vs Scissors -> Rock wins \n"
-      + "Paper vs Scissors -> Scissor wins \n")
+length = int(input("Enter password length: "))
 
-while True:
+print('''Choose character set for password from these : 
+		1. Digits
+		2. Letters
+		3. Special characters
+		4. Exit''')
 
-    print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
-
-
-
-    choice = int(input("Enter your choice :"))
+characterList = ""
 
 
+while (True):
+    choice = int(input("Pick a number "))
+    if (choice == 1):
 
 
-    while choice > 3 or choice < 1:
-        choice = int(input('Enter a valid choice please '))
+        characterList += string.ascii_letters
+    elif (choice == 2):
 
 
-    if choice == 1:
-        choice_name = 'Rock'
-    elif choice == 2:
-        choice_name = 'Paper'
-    else:
-        choice_name = 'Scissors'
+        characterList += string.digits
+    elif (choice == 3):
 
 
-    print('User choice is \n', choice_name)
-    print('Now its Computers Turn....')
-
-
-    comp_choice = random.randint(1, 3)
-
-
-    while comp_choice == choice:
-        comp_choice = random.randint(1, 3)
-
-
-    if comp_choice == 1:
-        comp_choice_name = 'rocK'
-    elif comp_choice == 2:
-        comp_choice_name = 'papeR'
-    else:
-        comp_choice_name = 'scissoR'
-    print("Computer choice is \n", comp_choice_name)
-    print(choice_name, 'Vs', comp_choice_name)
-
-    if choice == comp_choice:
-        print('Its a Draw', end="")
-        result = "DRAW"
-
-
-    if (choice == 1 and comp_choice == 2):
-        print('paper wins =>', end="")
-        result = 'papeR'
-    elif (choice == 2 and comp_choice == 1):
-        print('paper wins =>', end="")
-        result = 'Paper'
-
-    if (choice == 1 and comp_choice == 3):
-        print('Rock wins =>\n', end="")
-        result = 'Rock'
-    elif (choice == 3 and comp_choice == 1):
-        print('Rock wins =>\n', end="")
-        result = 'rocK'
-
-    if (choice == 2 and comp_choice == 3):
-        print('Scissors wins =>', end="")
-        result = 'scissoR'
-    elif (choice == 3 and comp_choice == 2):
-        print('Scissors wins =>', end="")
-        result = 'Rock'
-
-    if result == 'DRAW':
-        print("<== Its a tie ==>")
-    if result == choice_name:
-        print("<== User wins ==>")
-    else:
-        print("<== Computer wins ==>")
-    print("Do you want to play again? (Y/N)")
-
-    ans = input().lower
-    if ans == 'n':
+        characterList += string.punctuation
+    elif (choice == 4):
         break
+    else:
+        print("Please pick a valid option!")
 
-print("thanks for playing")
+password = []
+
+for i in range(length):
+
+    randomchar = random.choice(characterList)
+
+
+    password.append(randomchar)
+
+# printing password as a string
+print("The random password is " + "".join(password))
